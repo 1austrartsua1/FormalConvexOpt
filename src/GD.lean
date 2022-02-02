4114be9dbd8353da32dc3299405397f11d858f77
1austrartsua1 <patrick.r.johnstone@gmail.com>
2022-02-02 10:46:07 -0500

    preamble GD.lean

--- src/GD.lean
+++ src/GD.lean
@@ -1,6 +1,7 @@
 /-
 This file contains a formal computer proof of O(1/k) convergence of gradient descent with 
-constant stepsize for convex functions on a real inner-product space. It is self-contained other than using properties of real and natural numbers defined in mathlib.  
+constant stepsize for convex functions on a real inner-product space. It is 
+self-contained other than using properties of real and natural numbers defined in mathlib.  
 I define the properties of convexity and Lipschitz continuous gradient here
 and do not rely on the mathlib-defined gradient. This means that a user 
 would have to ensure that their function is convex and the gradient is correct.
